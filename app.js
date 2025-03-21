faa3500898a4b286dd544e4cb95e24c2de098483
lillian <keynesiane@gmail.com>
2025-03-21 16:07:30 -0700

    set credentials for cors true

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ const messageRouter = require('./routes/messagesRouter');
 const profileRouter = require('./routes/profilesRouter');
 
 app.use(logger('dev'));
-app.use(cors());
+app.use(cors(
+  {
+    credentials: true,
+  }
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
