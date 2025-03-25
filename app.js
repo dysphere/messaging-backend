5b03c27020f0fe7e4c06b31f179d80bdbd6d6e07
lillian <keynesiane@gmail.com>
2025-03-25 14:23:49 -0700

    cors options

--- app.js
+++ app.js
@@ -21,7 +21,13 @@ const messageRouter = require('./routes/messagesRouter');
 const profileRouter = require('./routes/profilesRouter');
 
 app.use(logger('dev'));
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true
+}));
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
